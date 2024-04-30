c6b1fd662ee1be6ed8fb3134a368c0311f41c94f
SooJ2 <2ecrystal@naver.com>
2024-04-30 10:04:24 +0900

    remane DFS and push BFS

--- week3/bfs_example.cpp
+++ week3/bfs_example.cpp
@@ -1,49 +1,50 @@
 #include <iostream>
-
+#include <queue>
 using namespace std;
+
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
-int N, M;
-int map[100][100]; int polluted_map [100][100];
+int visited[100][100];
+int map[100][100];
+int M,N,cy,cx;
 
 void BFS(int y, int x){
-  polluted_map[y][x] = 1;
-  cout << "(" <<  y << ", " << x << ")" << " polluted!!" << endl;
-  for(int i = 0; i < 4; i++){
-    int ny = y + dy[i];
-    int nx = x + dx[i];
-    cout << "\t()" << ny << ", " << nx << ") check" << endl;
-    if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-    if (map[ny][nx] == 0 || polluted_map[ny][nx] == 1) continue;
-    BFS(ny,nx);
+  queue<int> qy,qx;
+  qy.push(y); qx.push(x);
+  visited[y][x] = 1;
+
+  while(qy.size()){
+    int ty = qy.front(), tx = qx.front();
+    qy.pop(); qx.pop();
+    for (int i = 0; i < 4; i++)
+    {
+      int ny = ty + dy[i];
+      int nx = tx + dx[i];
+      if(ny < 0 || ny >= 100 || nx < 0 || nx >= 100) continue;
+      if(map[ny][nx] == 0) continue;
+      visited[ny][nx] = visited[ty][tx] + 1;
+      if(ny == cy && nx == cx){
+        cout << "[TOTAL] 당근 "<< visited[ny][nx] << "개 소모!" << endl;
+        return;
+      } 
+      qy.push(ny); qx.push(nx);
+    }
   }
+  
+
 }
 
 int main(){
-  int input;
   cin >> N >> M;
-  for(int i = 0; i < N; i++){
-    for(int j = 0; j < M; j++){
-      cin >> input;
-      map[i][j] = input;
-    }
-  }
-  //init
-  for(int i = 0; i < N; i++){
-    for(int j = 0; j < M; j++){
-      polluted_map[i][j] = 0;
-    }
-  }
-  int x,y,count = 0;
-  for (y = 0; y < N; y++){
-    for (x = 0; x < M; x++){
-      if(polluted_map[y][x] || !map[y][x]) continue;
-      cout << "------BFS!------" << endl;
-      BFS(y,x);
-      count ++;
-    }
-  }
 
-  cout << count << endl;
+  int y,x;
+  cin >> y >> x;
+  cin >> cy >> cx;
 
+  for (int i = 0; i < N; i++){
+    for (int j = 0; j < M; j++){
+      cin >> map[i][j];
+    }
+  }
+  BFS(y,x);
 }
---
+++ week3/dfs_example.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+using namespace std;
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+int N, M;
+int map[100][100]; int polluted_map [100][100];
+
+void DFS(int y, int x){
+  polluted_map[y][x] = 1;
+  cout << "(" <<  y << ", " << x << ")" << " polluted!!" << endl;
+  for(int i = 0; i < 4; i++){
+    int ny = y + dy[i];
+    int nx = x + dx[i];
+    cout << "\t()" << ny << ", " << nx << ") check" << endl;
+    if (ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+    if (map[ny][nx] == 0 || polluted_map[ny][nx] == 1) continue;
+    DFS(ny,nx);
+  }
+}
+
+int main(){
+  int input;
+  cin >> N >> M;
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      cin >> input;
+      map[i][j] = input;
+    }
+  }
+  //init
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      polluted_map[i][j] = 0;
+    }
+  }
+  int x,y,count = 0;
+  for (y = 0; y < N; y++){
+    for (x = 0; x < M; x++){
+      if(polluted_map[y][x] || !map[y][x]) continue;
+      cout << "------DFS!------" << endl;
+      DFS(y,x);
+      count ++;
+    }
+  }
+
+  cout << count << endl;
+
+}
