096e2bdb3104fdfb4c99915938c6aebce6dc5b11
SooJ2 <2ecrystal@naver.com>
2024-05-23 12:01:05 +0900

    1450 tmp push

---
+++ week3/14502.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+
+// #define SAFE 0;
+// #define WALL 1;
+// #define VIRUS 2;
+
+using namespace std;
+
+int N,M,maximum = 0;
+int map[8][8];
+int dy[4] = {-1,0,1,0};
+int dx[4] = {0,1,0,-1};
+
+int find_safe_area(int** polluted_map){
+  // cout << "FIND SAFE AREA" << endl;
+  int safe_area = 0;
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      if(polluted_map[i][j] == 0) safe_area++;
+      // cout << polluted_map[i][j] << " ";
+    }
+    // cout << endl;
+  }
+  // cout << endl;
+  // cout << "END FIND SAFE AREA" << endl;
+
+  return safe_area;
+}
+
+void DFS(int y, int x, int ** polluted_map){
+  // cout << "DFS" << endl;
+  // cout << "(" << y << "," << x << ")"<<endl;
+  polluted_map[y][x] = 2;
+  for(int i = 0; i < 4; i++){
+    int ny,nx;
+    ny = y + dy[i]; nx = x + dx[i];
+    if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
+    if(polluted_map[ny][nx] != 0) continue;
+    DFS(ny,nx,polluted_map);
+  }
+  // for(int i = 0; i < N; i++){
+  //   for(int j = 0; j < M; j++){
+  //     cout << polluted_map[i][j] << " ";
+  //   }
+  //   cout << endl;
+  // }
+  // cout << endl;
+}
+
+void pollute(){
+  // cout << "POLLUTE" << endl;
+  int **polluted_map = new int*[8];
+  for(int i = 0; i < N; i++){
+    polluted_map[i] = new int[8];
+    for(int j = 0; j < M; j++){
+      polluted_map[i][j] = map[i][j];
+    }
+  }
+
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      if(map[i][j] != 2) continue;
+      // cout << "START DFS" << endl;
+      DFS(i,j,polluted_map);
+      // cout << "END DFS" << endl;
+    }
+  }
+
+  int safe_area = find_safe_area(polluted_map);
+  // cout << safe_area << endl;
+  if(safe_area > maximum) maximum = safe_area;
+  return;
+}
+
+void find_solution(int walls){
+  if(walls == 3){
+    pollute();
+    return;
+  }
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      if (map[i][j] != 0) continue;
+      map[i][j] = 1;
+      find_solution(walls+1);
+      map[i][j] = 0;
+    }
+  }
+
+}
+
+int main(){
+  cin >> N >> M;
+  for(int i = 0; i < N; i++){
+    for(int j = 0; j < M; j++){
+      cin >> map[i][j];
+    }
+  }
+
+  // cout << "FIND SOLUTION" << "\n";
+  find_solution(0);
+  // cout << "END FIND SOLUTION" << "\n";
+
+  cout << maximum << "\n";
+}
+
+/*
+0 0 0 0 0 0
+1 0 0 0 0 2
+1 1 1 0 0 2
+0 0 0 0 0 2
+*/
