2dd1ec5645160e26dcaf41b93fdb7902e70dd0a6
SooJ2 <2ecrystal@naver.com>
2024-05-28 16:11:02 +0900

    1325 tmp

--- week2/1325.cpp
+++ week2/1325.cpp
@@ -2,40 +2,36 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 int N,M;
-map<int,vector<int>> relationship;
-map<int,int> child_map;
+// map<int,vector<int>> relationship;
+map<int,set<int>> relationship;
+map<int,set<int>> parents;
+// map<int,int> child_map;
 int check[10000];
 int maximum = 0;
 vector<int> IDs;
 
 
-int find_childs(int node,bool start){
-  cout <<  node << endl;
-  check[node] = 1;
-  // if(relationship[node].size() == 0) return 1;
-  // if(find(relationship.begin(),relationship.end(),node) == relationship.end()) return 1;
-  if(relationship.find(node) == relationship.end()) return 1;
+// int find_childs(int node,bool start){
+//   check[node] = 1;
+
+//   if(relationship.find(node) == relationship.end()) return 1;
   
-  int childs;
-  if(start) childs = 0;
-  else childs = 1;
+//   int childs;
+//   if(start) childs = 0;
+//   else childs = 1;
 
-  // for(int i = 0; i < relationship[node].size(); i++){
-  //   if(check[i]) continue;//visited
-  //   cout << "VISIT "<<relationship[node][i] << endl;
-  //   childs += find_childs(relationship[node][i],false);
-  // }
 
-  for(int i: relationship[node]){
-    if(check[i]) continue;
-    childs += find_childs(i,false);
-  }
+//   for(int i: relationship[node]){
+//     if(check[i]) continue;
+//     childs += find_childs(i,false);
+//   }
 
-  return childs;
-}
+//   return childs;
+// }
 
 int main(){
   cin >> N >> M; // #N computer, #M input
@@ -44,58 +40,72 @@ int main(){
     cin >> A >> B;
     M--;
     if(!check[B]){
-      vector<int> v;
-      v.push_back(A);
-      relationship[B] = v;
+      set<int> s;
+      s.insert(A);
+      relationship[B] = s;
       check[B] = 1;
-      continue;
+      
+    }else{
+      relationship[B].insert(A);
     }
-    relationship[B].push_back(A);
+    if(check[A]){
+      for(int i : relationship[A]){
+        relationship[B].insert(i);
+      }
+    }
+    
+    if(parents.find(A)==parents.end()){
+      parents[A] = {B};
+    }else{
+      parents[A].insert(B);
+    }
+
+    if(check[B]){
+      for(int i : parents[B]){
+        relationship[i].insert(A);
+      }
+    }
   }
-
-  //추가
-  // for(int i = 0; i < relationship.size(); i++){
-  //   fill_n(check,10000,0);
-  //   cout << "["<<<"]"<<endl;
-  //   child_map[i] = find_childs(i,true);
-  // }
-
-  for(pair p: relationship){
-    fill_n(check,10000,0);
-    cout << "[" << p.first << "] ";
-    child_map[p.first] = find_childs(p.first,true);
-  }
-
-
-  for(pair p: relationship){
-    cout << p.first << "] ";
-    for(int i:p.second){
-      cout << i << " ";
-    }cout << endl;
-  }
-  cout << endl;
-  for(pair p: child_map){
-    cout << p.first << "] " << p.second <<endl;
-  }
-
 
   // for(pair p: relationship){
-  //   cout << p.first << "] SIZE: " << p.second.size() << endl;
-  //   for(int i:p.second){
+  //   cout << p.first << "] " << p.second.size() << endl;
+  //   for(int i : p.second){
   //     cout << i << " ";
   //   }cout << endl;
-  //   if(p.second.size() > maximum){
-  //     maximum = p.second.size();
+  //   if(p.second.size()>maximum){
   //     IDs.clear();
+  //     maximum = p.second.size();
   //     IDs.push_back(p.first);
   //   }else if(p.second.size() == maximum){
   //     IDs.push_back(p.first);
   //   }
   // }
 
-  // for(int ID: IDs){
-  //   cout << ID << " ";
+  // for(pair p: relationship){
+  //   fill_n(check,10000,0);
+  //   child_map[p.first] = find_childs(p.first,true);
+  //   if(child_map[p.first] == maximum) IDs.push_back(p.first);
+  //   else if(child_map[p.first] > maximum){
+  //     maximum = child_map[p.first];
+  //     IDs.clear();
+  //     IDs.push_back(p.first);
+  //   }
   // }
-  // cout << "\n";
+
+  for(pair p: relationship){
+    if(p.second.size()>maximum){
+      IDs.clear();
+      maximum = p.second.size();
+      IDs.push_back(p.first);
+    }else if(p.second.size() == maximum){
+      IDs.push_back(p.first);
+    }
+  }
+
+
+  for(int ID: IDs){
+    cout << ID << " ";
+  }
+  cout << "\n";
 
 }
--- week2/tempCodeRunnerFile.cpp
+++ week2/tempCodeRunnerFile.cpp
@@ -1,2 +1,2 @@
 
-  cout << ROT13(input);
+    if(check[A]){
