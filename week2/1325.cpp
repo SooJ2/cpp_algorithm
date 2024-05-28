2b323b27d181e8db94c3ccba025754891e2921cf
SooJ2 <2ecrystal@naver.com>
2024-05-28 17:24:05 +0900

    1325 complete

--- week2/1325.cpp
+++ week2/1325.cpp
@@ -1,111 +1,51 @@
 #include <iostream>
 #include <vector>
-#include <map>
+// #include <map>
 #include <algorithm>
-#include <set>
+#include <string.h>
 
 using namespace std;
 int N,M;
-// map<int,vector<int>> relationship;
-map<int,set<int>> relationship;
-map<int,set<int>> parents;
-// map<int,int> child_map;
-int check[10000];
+int child_map[10001];
+int check[10001];
 int maximum = 0;
-vector<int> IDs;
+int A,B;
+vector<int> relationship[10001];
 
 
-// int find_childs(int node,bool start){
-//   check[node] = 1;
-
-//   if(relationship.find(node) == relationship.end()) return 1;
-  
-//   int childs;
-//   if(start) childs = 0;
-//   else childs = 1;
-
-
-//   for(int i: relationship[node]){
-//     if(check[i]) continue;
-//     childs += find_childs(i,false);
-//   }
-
-//   return childs;
-// }
+int find_childs(int node){
+  check[node] = 1;
+  int childs = 1;
+  for(int i: relationship[node]){
+    if(check[i]) continue;
+    childs += find_childs(i);
+  }
+  return childs;
+}
 
 int main(){
+
+  ios_base::sync_with_stdio(0); 
+  cin.tie(NULL); cout.tie(NULL);
   cin >> N >> M; // #N computer, #M input
-  while(M){
-    int A,B;
+  while(M--){
     cin >> A >> B;
-    M--;
-    if(!check[B]){
-      set<int> s;
-      s.insert(A);
-      relationship[B] = s;
-      check[B] = 1;
-      
-    }else{
-      relationship[B].insert(A);
-    }
-    if(check[A]){
-      for(int i : relationship[A]){
-        relationship[B].insert(i);
-      }
-    }
-    
-    if(parents.find(A)==parents.end()){
-      parents[A] = {B};
-    }else{
-      parents[A].insert(B);
-    }
-
-    if(check[B]){
-      for(int i : parents[B]){
-        relationship[i].insert(A);
-      }
-    }
-  }
-
-  // for(pair p: relationship){
-  //   cout << p.first << "] " << p.second.size() << endl;
-  //   for(int i : p.second){
-  //     cout << i << " ";
-  //   }cout << endl;
-  //   if(p.second.size()>maximum){
-  //     IDs.clear();
-  //     maximum = p.second.size();
-  //     IDs.push_back(p.first);
-  //   }else if(p.second.size() == maximum){
-  //     IDs.push_back(p.first);
-  //   }
-  // }
-
-  // for(pair p: relationship){
-  //   fill_n(check,10000,0);
-  //   child_map[p.first] = find_childs(p.first,true);
-  //   if(child_map[p.first] == maximum) IDs.push_back(p.first);
-  //   else if(child_map[p.first] > maximum){
-  //     maximum = child_map[p.first];
-  //     IDs.clear();
-  //     IDs.push_back(p.first);
-  //   }
-  // }
-
-  for(pair p: relationship){
-    if(p.second.size()>maximum){
-      IDs.clear();
-      maximum = p.second.size();
-      IDs.push_back(p.first);
-    }else if(p.second.size() == maximum){
-      IDs.push_back(p.first);
-    }
+    relationship[B].push_back(A);
   }
 
 
-  for(int ID: IDs){
-    cout << ID << " ";
+  for(int i = 1; i <= N; i++){
+    // fill_n(check,10000,0);
+    memset(check,0,sizeof(check));
+    child_map[i] = find_childs(i);
+    maximum = max(child_map[i],maximum);
   }
-  cout << "\n";
 
+
+
+  for(int i = 1; i <= N; i++){
+    if(maximum == child_map[i]) cout << i << " ";
+  }
+
+  return 0;
 }
--- week2/tempCodeRunnerFile.cpp
+++ week2/tempCodeRunnerFile.cpp
@@ -1,2 +1,55 @@
+#include <iostream>
+#include <vector>
+// #include <map>
+#include <algorithm>
+#include <string.h>
 
-    if(check[A]){
+using namespace std;
+int N,M;
+// map<int,vector<int>> relationship;
+// map<int,int> child_map;
+int child_map[10000];
+int check[10000];
+int maximum = 0;
+// vector<int> IDs;
+vector<int> relationship[10000];
+
+
+int find_childs(int node){
+  int childs = 1;
+  for(int i: relationship[node]){
+    if(check[i]) continue;
+    check[node] = 1;
+    childs += find_childs(i);
+  }
+
+  return childs;
+}
+
+int main(){
+
+  ios_base::sync_with_stdio(0); cin.tie(0);
+
+  cin >> N >> M; // #N computer, #M input
+  while(M--){
+    int A,B;
+    cin >> A >> B;
+    relationship[B].push_back(A);
+  }
+
+
+  for(int i = 1; i <= N; i++){
+    // fill_n(check,10000,0);
+    memset(check,0,sizeof(check));
+    child_map[i] = find_childs(i);
+    maximum = max(child_map[i],maximum);
+  }
+
+
+
+  for(int i = 1; i <= N; i++){
+    if(maximum == child_map[i]) cout << i << " ";
+  }
+
+  return 0;
+}
