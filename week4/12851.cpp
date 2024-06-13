86dea848b576631474bd567a2ddb9a3e3616494b
SooJ2 <2ecrystal@naver.com>
2024-06-13 13:53:49 +0900

    fin: Add second solution

--- week4/12851.cpp
+++ week4/12851.cpp
@@ -38,4 +38,75 @@ int main(){
   find_answer();
   cout << visited[K]-1 << "\n" << cnt[K];
   return 0;
+}//3192KB
+
+/*
+#include <iostream>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+int N, K, answer_depth;
+long long answer_count;
+long long visited[100001];
+int visited_depth[100001];
+
+void find_shortest(){
+  queue<pair<int,int>> q;
+
+  visited[N] = 1;
+  visited_depth[N] = 0;
+  int depth = 0, curr = N;
+  q.push({depth,curr});
+  while(q.size()){
+    tie(depth,curr) = q.front(); q.pop();
+    if(curr == K){
+      answer_depth = depth;
+      answer_count = visited[curr];
+      return;
+    }else{
+      if((curr-1) >= 0){
+        if(visited[curr-1] && visited_depth[curr-1] == depth+1){
+          visited[curr-1]+=visited[curr];
+        }
+        else if (!visited[curr-1]){
+          visited[curr-1] = visited[curr];
+          visited_depth[curr-1] = depth+1;
+          q.push({depth+1,curr-1});
+        }
+      }
+      if((curr+1) <= 100000){
+        if(visited[curr+1] && visited_depth[curr+1] == depth+1){
+          visited[curr+1]+=visited[curr];
+        }
+        else if (!visited[curr+1]){
+          visited[curr+1] = visited[curr];
+          visited_depth[curr+1] = depth+1;
+          q.push({depth+1,curr+1});
+        }
+      }
+      if(curr*2 <= 100000){
+        if(visited[curr*2] && visited_depth[curr*2] == depth+1){
+          visited[curr*2] += visited[curr];
+        }
+        else if(!visited[curr*2]){
+          visited[curr*2] = visited[curr];
+          visited_depth[curr*2] = depth+1;
+          q.push({depth+1,curr*2});
+        }
+      }
+    }
+  }
 }
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+  cin >> N >> K;
+
+  find_shortest();
+
+  cout << answer_depth << "\n" << answer_count << "\n";
+}//3456KB
+
+*/
