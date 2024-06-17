5d9a2fdc3ad4436f417e0fe2925490644792451f
SooJ2 <2ecrystal@naver.com>
2024-06-17 16:56:47 +0900

    fin: 14497

--- week4/14497.cpp
+++ week4/14497.cpp
@@ -10,25 +10,16 @@ int class_map[300][300];
 int visited[300][300];
 
 bool find_answer(int y, int x){
-  // if(y==ty && x == tx){
-
-  //   return false;
-  // }
-  // cout << "JUSUNG " << jy << " " << jx << "\n";
   int nx, ny;
   for(int i = 0; i < 4; i++){
     ny = y + dy[i]; nx = x + dx[i];
-    // cout << "[" << ny << "," << nx << "]\n";
     if(ny < 0 || ny >= N || nx < 0 || nx >= M) continue;
-    // cout << "not overflow\n";
     if(visited[ny][nx]) continue;
-    // cout << "not visited\n";
     if(class_map[ny][nx] == 4) return false;
     
     visited[ny][nx] = 1;
     if(class_map[ny][nx] == 1){
-      // cout << "FRIEND " << ny << " " << nx << "\n";
-      class_map[ny][nx] = 0; //visited 처리
+      class_map[ny][nx] = 0;
       continue;
     }
     if(!find_answer(ny,nx)) return false;
@@ -37,16 +28,11 @@ bool find_answer(int y, int x){
 }
 
 int main(){
+  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
   cin >> N >> M;
-  // cout << "\n" << N << " " << M << "\n";
 
   cin >> jy >> jx >> ty >> tx;
   jx--;jy--;tx--;ty--;
-
-  // cout << "\n";
-  // cout << jy << " " << jx << "\n";
-  // cout << ty << " " << tx << "\n";
-  // cout << "\n";
 
   string input;
   for(int i = 0; i < N; i++){
@@ -58,13 +44,6 @@ int main(){
     }
   }
 
-  // for(int i = 0; i < N; i++){
-  //   for(int j = 0; j < M; j++){
-  //     cout << class_map[i][j];
-  //   }cout << "\n";
-  // }
-  // cout << "input fin\n";
-
   if(jy == ty && jx == tx) return 0;
 
   bool cont = true;
@@ -74,12 +53,6 @@ int main(){
     visited[jy][jx] = 1;
     cont = find_answer(jy,jx);
     answer++;
-    // cout << answer << "\n";
-    // for(int i = 0; i < N; i++){
-    //  for(int j = 0; j < M; j++){
-    //     cout << class_map[i][j];
-    //   }cout << "\n";
-    // } 
   }
   cout << answer;
-}
+}//64ms
