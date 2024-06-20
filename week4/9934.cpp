71abcebe7d88f2434be9f73475e204570d8d2e01
SooJ2 <2ecrystal@naver.com>
2024-06-20 16:38:21 +0900

    init: 9934

---
+++ week4/9934.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <tuple>
+
+using namespace std;
+
+int K;
+int *buildings;
+int visited[1025];
+queue<pair<int,int>> answer;
+//1 ~ 2^K-1
+
+void find_answer(int left, int right){
+  answer.push({left,right});
+  // int i, mid_l, mid_r ,
+  int answer_size = answer.size();
+  int l, r, mid;
+  while(answer_size){
+    tie(l, r) = answer.front(); answer.pop();
+    answer_size--;
+    mid = l + int(r-l)/2; 
+    // cout << "[" << l << ", " << r << "] " << mid << "\n";
+    //visited
+    if(visited[mid]) continue;
+
+    visited[mid] = 1;
+    cout << buildings[mid] << " ";
+    if(l == r) continue;
+
+    answer.push({l,mid-1});// [)
+    answer.push({mid+1,r});
+
+  
+    if(answer_size == 0){
+      cout << "\n";
+      answer_size = answer.size();
+    }
+  }
+}
+
+
+int main(){
+  cin >> K;
+  buildings = new int[K];
+  for(int i = 0; i < pow(2,K)-1; i++){
+    cin >> buildings[i];
+  }
+
+  find_answer(0,pow(2,K)-2);
+}
+
+/*
+
+5
+0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+
+*/
